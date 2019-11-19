8f276c507707a038e2b5e04fe50fe420f119828f
tubone24 <tubo.yyyuuu@gmail.com>
2019-11-19 23:26:53 +0900

    add testcase and fix seq test

--- tests/testsubmodule.nim
+++ tests/testsubmodule.nim
@@ -1,9 +1,11 @@
 import unittest
-import base64, os, json
+import base64, os, json, algorithm
 
 import ebook_homebrew_nim_clientpkg/submodule
 test "listImgFile":
-  check listImgFile("tests/assets") == @["tests\\assets\\saved_file1570019388166.jpg", "tests\\assets\\saved_file1570022912069.jpg"]
+  # you can get like those seq@["tests\\assets\\saved_file1570019388166.jpg", "tests\\assets\\saved_file1570022912069.jpg"]
+  check "tests\\assets\\saved_file1570019388166.jpg" in listImgFile("tests/assets") 
+  check "tests\\assets\\saved_file1570022912069.jpg" in listImgFile("tests/assets") 
 
 test "convertBase64":
   let f : File = open("tests/assets/saved_file1570022912069.jpg" , FileMode.fmRead)
@@ -11,6 +13,18 @@ test "convertBase64":
     close(f)
   let base64 = encode(f.readAll())
   check convertBase64("tests/assets/saved_file1570022912069.jpg") == base64
+
+test "listImgFiles":
+  let actual = listImgFiles("tests/assets")
+  let f1 : File = open("tests/assets/saved_file1570022912069.jpg" , FileMode.fmRead)
+  let f2 : File = open("tests/assets/saved_file1570019388166.jpg" , FileMode.fmRead)
+  defer:
+    close(f1)
+    close(f2)
+  let base64one = encode(f1.readAll())
+  let base64two = encode(f2.readAll())
+  check base64one in actual
+  check base64two in actual
 
 test "extractUploadId":
   check extractUploadId("{\"upload_id\": \"test\", \"aaa\": \"bbb\"}") == "test"
